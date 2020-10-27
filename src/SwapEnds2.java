e6a61119d8f3d59d9d4b7a6745e3cf2a241e68e6
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-27 06:14:12 -0600

    Initial commit

---
+++ src/SwapEnds2.java
@@ -0,0 +1,9 @@
+public class SwapEnds2 {
+  public int[] swapEnds(int[] nums) {
+    int temp = 0;
+    temp = nums[0];
+    nums[0] = nums[nums.length - 1];
+    nums[nums.length -1] = temp;
+    return nums;
+  }
+}
